172cb16e10bcbe77c716f70c180ab8b7e6b9e03f
joseph-sharick <124623900+joseph-sharick@users.noreply.github.com>
2023-10-12 14:49:07 -0400

    Update new.js

--- new.js
+++ new.js
@@ -1,48 +1,10 @@
 let a = 10;
 let b = 20;
 
-function add(first, second) {
-  first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-    first+=1;
-  first+=1;
-  return first;
-}
-function nestedComplexity(a,b,c,d,e,f,g,h,i,j,k,l,m){
-   if(a<b) {
-     if (b<c){
-       if (c<d){
-         if(d<e){if(e<f){}else{}}else{}
-       } else{}
-     }
-     else{}
-   }
-  else {
-  }
-}
 function addition(first, second) {
   return first + second;
 }
 
-add(a,b);
 addition(a,b);
 //SonarCloud should analyze this...
 
