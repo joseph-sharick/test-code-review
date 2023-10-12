c5a5ac76e6076118a078bce368f446834200df69
joseph-sharick <124623900+joseph-sharick@users.noreply.github.com>
2023-10-12 14:42:21 -0400

    Update new.js

--- new.js
+++ new.js
@@ -1,13 +1,15 @@
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
-console.log("Here is some duplicated javascript code");
+let a = 10;
+let b = 20;
+
+function add(first, second) {
+  return first + second;
+}
+
+function addition(first, second) {
+  return first + second;
+}
+
+add(a,b);
+addition(a,b);
 //SonarCloud should analyze this...
 
